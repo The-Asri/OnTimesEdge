9c0209b211375e730ce137be313385ff65b0cda5
BuildTools <unconfigured@null.spigotmc.org>
2023-03-11 13:50:04 +0100

    box file

--- box.py
+++ box.py
@@ -16,4 +16,8 @@ class Box:
         pygame.draw.rect(s, "Red", (self.x - self.width/2 - c.x, self.y - self.height/2 - c.y, self.width, self.height))
 
     def is_colliding(self, b):
-        pass
+        for point in b.getPoints():
+            if self.x - self.width / 2 <= point[0] <= self.x + self.width / 2 and \
+                    self.y - self.height / 2 <= point[1] <= self.y + self.height / 2:
+                return True
+        return False
