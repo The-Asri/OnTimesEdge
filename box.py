54a9e06fd72cc2899354a067dae533ed62693d1b
MrKapino <117902509+MrKapino@users.noreply.github.com>
2023-03-11 13:56:09 +0100

    box commit

---
+++ box.py
@@ -0,0 +1,19 @@
+import pygame.surface
+
+
+class Box:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def getPoints(self):
+        return [[self.x - self.width/2, self.y - self.height/2], [self.x + self.width/2, self.y - self.height/2],
+                [self.x - self.width/2, self.y + self.height/2], [self.x + self.width/2, self.y + self.height/2]]
+
+    def draw(self, s, c):
+        pygame.draw.rect(s, "Red", (self.x - self.width/2 - c.x, self.y - self.height/2 - c.y, self.width, self.height))
+
+    def is_colliding(self, b):
+        pass
