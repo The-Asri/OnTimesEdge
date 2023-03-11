a58697d74c307a2a96f8fd6e5c0a1dd9711b094b
MrKapino <117902509+MrKapino@users.noreply.github.com>
2023-03-11 12:48:44 +0100

    bild

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import eventHandler
 import KeyManager
 
 screen = None
+surface = None
 clock = None
 keyManager = None
+trueWidth = 150
+trueHeight = 100
+upscale = 8
+width = trueWidth * upscale
+height = trueHeight * upscale
 
 
 def init():
     global screen
     global clock
     global keyManager
+    global surface
+
     pygame.init()
-    screen = pygame.display.set_mode((600, 500))
+    screen = pygame.display.set_mode((width, height))
+    surface = pygame.Surface((trueWidth, trueHeight))
     pygame.display.set_caption("GameJam")
     clock = pygame.time.Clock()
     keyManager = KeyManager.KeyManager()
@@ -20,13 +29,21 @@ def init():
 
 def main():
     eventHandler.eventHandler(keyManager)
-    #
-    #
+    update()
+    draw()
     pygame.display.update()
     clock.tick(30)
+    if keyManager.key_jump:
+        print("jump")
 
+def update():
+    pass
+def draw():
+    surface.blit(testImage, (20,20))
+    upscaled = pygame.transform.scale_by(surface, upscale)
+    screen.blit(upscaled,(0,0))
 
 init()
-
+testImage = pygame.image.load("graphics\\schneebimer.png")
 while True:
     main()
