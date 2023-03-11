b23abc218ba45ad5dc781d56458682b9c261d83b
Tom Crow <TomCrowBeastgang@gmail.com>
2023-03-11 12:21:25 +0100

    input

---
+++ KeyManager.py
@@ -0,0 +1,40 @@
+import pygame
+
+class KeyManager:
+    def __init__(self):
+        self.key_left = False
+        self.key_right = False
+        self.key_jump = False
+        self.key_switch = False
+        self.key_reset = False
+        self.key_escape = False
+
+    def keyDown(self, key):
+        if key == pygame.K_a:
+            self.key_left = True
+        if key == pygame.K_d:
+            self.key_right = True
+        if key == pygame.K_SPACE:
+            self.key_jump = True
+        if key == pygame.K_e:
+            self.key_switch = True
+        if key == pygame.K_r:
+            self.key_reset = True
+        if key == pygame.K_ESCAPE:
+            self.key_escape = True
+
+    def keyUp(self, key):
+        if key == pygame.K_a:
+            self.key_left = False
+        if key == pygame.K_d:
+            self.key_right = False
+        if key == pygame.K_SPACE:
+            self.key_jump = False
+        if key == pygame.K_e:
+            self.key_switch = False
+        if key == pygame.K_r:
+            self.key_reset = False
+        if key == pygame.K_ESCAPE:
+            self.key_escape = False
+
+
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 import eventHandler
+import KeyManager
 
 screen = None
 clock = None
-
+keyManager = None
 
 def init():
     global screen
     global clock
+    global keyManager
     pygame.init()
     screen = pygame.display.set_mode((600, 500))
     pygame.display.set_caption("GameJam")
     clock = pygame.time.Clock()
+    keyManager = KeyManager.KeyManager()
 
 
 def main():
-    eventHandler.eventHandler()
+    eventHandler.eventHandler(keyManager)
+    if keyManager.key_jump:
+        print("Jumping!")
     #
     #
     pygame.display.update()
