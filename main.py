989cfa599010c6307666a8ed634f9dae4fb2f9ad
BuildTools <unconfigured@null.spigotmc.org>
2023-03-15 14:51:20 +0100

    sound fix

--- main.py
+++ main.py
@@ -116,7 +116,6 @@ def update():
                 ticking = True
 
         if keyManager.key_reset and not resetLock:
-            soundHandler.playSound("jump")
             reset()
         if keyManager.key_switch and not switchLock:
             switch()
@@ -194,9 +193,12 @@ def reset():
     global resetLock
 
     resetLock = True
-    soundHandler.playSound("death")
 
     LevelAssets.loadLevel(currentLevel, boxesCity, boxesRuins, levelBorder, cityBackground, ruinsBackground, player)
+    if player.isDead:
+        soundHandler.playSound("death")
+    else:
+        soundHandler.playSound("reset")
     inCity = True
     currentBoxes = boxesCity
     player.isDead = False
